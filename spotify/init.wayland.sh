37456952a0cbc43da53d3dd47a22d11978762143
Wagner Riffel <wgrriffel@gmail.com>
2019-11-19 17:52:13 +0000

    spotify: fix init script

--- spotify/init.wayland.sh
+++ spotify/init.wayland.sh
@@ -2,14 +2,13 @@
 
 docker run -d \
 	-e DISPLAY=$DISPLAY \
+	-v spotify:/home/spotify \
 	-e PULSE_SERVER=/tmp/pulse/native \
 	-v /tmp/.X11-unix:/tmp/.X11-unix \
 	-v /tmp/wgr/xdg/pulse:/tmp/pulse \
 	-v /etc/localtime:/etc/localtime:ro \
 	-v /dev/snd:/dev/snd \
-	-v spotify:/home/spotify/.config/spotify \
-	-v spotify2:/home/spotify/spotify \
 	--ipc=host \
 	--name spotify \
-	wgr/spotify
+	wgr/wspotify
 
