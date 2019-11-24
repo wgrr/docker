8d15aca097089c5555340bfbdf2025111f705b70
Wagner Riffel <wgrriffel@gmail.com>
2019-11-24 20:18:27 +0000

    telegram: rewrite share directory
    Signed-off-by: Wagner Riffel <wgrriffel@gmail.com>

--- telegram/init.wayland.sh
+++ telegram/init.wayland.sh
@@ -4,7 +4,7 @@ docker run -d \
 	-e DISPLAY=$DISPLAY \
 	-e PULSE_SERVER=/tmp/pulse \
 	-v telegram:/home/wgr \
-	-v $HOME/share/discord:/home/wgr/share \
+	-v $HOME/share/telegram:/home/wgr/share \
 	-v /etc/localtime:/etc/localtime:ro \
 	-v /tmp/.X11-unix:/tmp/.X11-unix \
 	-v $XDG_RUNTIME_DIR/pulse:/tmp/pulse \
